dc34d6441aa8ce8425ee7ceebf29d2f88389ce72
Andrew Berezovskyi <andriib@kth.se>
2024-10-12 14:57:55 +0200

    chore: reformat all with prettier
    Signed-off-by: Andrew Berezovskyi <andriib@kth.se>

--- frontend/configprovider.js
+++ frontend/configprovider.js
@@ -1,32 +1,11 @@
 var nconf = require('nconf');
 
-var ConfigProvider = function(path){
-	nconf.use('file', { file: path });
-	nconf.load();
-};
-	  
-ConfigProvider.prototype.save = function (){
-	nconf.save(function (err) {
-    if (err) {
-      console.error(err.message);
-      return;
-    }
-    console.log('Configuration saved successfully.');
-  });
+var ConfigProvider = function (path) {
+  nconf.use('file', { file: path });
+  nconf.load();
 };
 
-ConfigProvider.prototype.get = function(key) {
-  return nconf.get(key);
-};
-
-
-exports.ConfigProvider = ConfigProvider;
-	   
-	   
-  //
-  // Save the configuration object to disk
-  //
-  /*
+ConfigProvider.prototype.save = function () {
   nconf.save(function (err) {
     if (err) {
       console.error(err.message);
@@ -34,4 +13,25 @@ exports.ConfigProvider = ConfigProvider;
     }
     console.log('Configuration saved successfully.');
   });
-  */
+};
+
+ConfigProvider.prototype.get = function (key) {
+  return nconf.get(key);
+};
+
+
+exports.ConfigProvider = ConfigProvider;
+
+
+//
+// Save the configuration object to disk
+//
+/*
+nconf.save(function (err) {
+  if (err) {
+    console.error(err.message);
+    return;
+  }
+  console.log('Configuration saved successfully.');
+});
+*/
--- frontend/performanceView.js
+++ frontend/performanceView.js
@@ -1,38 +1,38 @@
 var MongoClient = require('mongodb').MongoClient
   , assert = require('assert');
 
- 
+
 // Connection URL 
 var url = 'mongodb://localhost:27017/sparqles';
 // Use connect method to connect to the Server 
-MongoClient.connect(url, function(err, db) {
+MongoClient.connect(url, function (err, db) {
   assert.equal(null, err);
   console.log("Connected correctly to server");
 
   var collection = db.collection('endpoints');
 
   // get list of endpoints
-  collection.find({}).toArray(function(err, endpoints) {
+  collection.find({}).toArray(function (err, endpoints) {
     //endpoints = endpoints.slice(0, 2); // first 5 endpoints just to test
     var results = {};
     function run(idx) {
-      if(idx >= endpoints.length) {
+      if (idx >= endpoints.length) {
         parseResults(db, results);
         return;
       }
 
-		  var endpoint = endpoints[idx];
+      var endpoint = endpoints[idx];
 
       // for each endpoint, get the last 10 performance results
-      console.log('running ptasks query with uri: '+ endpoint.uri);
+      console.log('running ptasks query with uri: ' + endpoint.uri);
 
       db.collection('ptasks').find({ "endpointResult.endpoint.uri": endpoint.uri })
-                .sort({ "endpointResult.end" : -1})
-                .limit(10)
-                .toArray(function(err, perResults) {
-                  results[endpoint.uri] = perResults;
-                  run(idx + 1)
-                })
+        .sort({ "endpointResult.end": -1 })
+        .limit(10)
+        .toArray(function (err, perResults) {
+          results[endpoint.uri] = perResults;
+          run(idx + 1)
+        })
     }
     run(0); // recursion, yuk
   });
@@ -43,9 +43,9 @@ function parseResults(db, results) {
   var ASKcold = [];
   var JOINwarm = [];
   var JOINcold = [];
-  for(var endpoint in results) {
+  for (var endpoint in results) {
     var arr = results[endpoint];
-    for(var i=0; i<arr.length; i++) {
+    for (var i = 0; i < arr.length; i++) {
       var obj = arr[i];
       ASKcold.push(obj.results.ASKS.cold.exectime);
       ASKcold.push(obj.results.ASKP.cold.exectime);
@@ -77,40 +77,40 @@ function parseResults(db, results) {
     median_ASK_warm: median(ASKwarm),
     median_ASK_cold: median(ASKcold),
     median_JOIN_warm: median(JOINwarm),
-    median_JOIN_cold:  median(JOINcold),
+    median_JOIN_cold: median(JOINcold),
     date_calculated: new Date()
   };
 
-  performance_widget.insert(obj, function(err, result) {
-    if(!err) console.log('successfully added data to performance_widget');
+  performance_widget.insert(obj, function (err, result) {
+    if (!err) console.log('successfully added data to performance_widget');
     db.close();
   });
 }
 
 function average(elmt) {
   var sum = 0;
-  for( var i = 0; i < elmt.length; i++ ){
-      sum += parseInt( elmt[i], 10 ); //don't forget to add the base
+  for (var i = 0; i < elmt.length; i++) {
+    sum += parseInt(elmt[i], 10); //don't forget to add the base
   }
 
-  var avg = sum/elmt.length;
+  var avg = sum / elmt.length;
   return avg;
 }
 function median(values) {
   var arr = []
-  for(var i in values) {
-    if(values[i] == 0) continue;
+  for (var i in values) {
+    if (values[i] == 0) continue;
     arr.push(values[i])
   }
 
   values = arr;
 
-  values.sort( function(a,b) {return a - b;} );
+  values.sort(function (a, b) { return a - b; });
 
-  var half = Math.floor(values.length/2);
+  var half = Math.floor(values.length / 2);
 
-  if(values.length % 2)
-      return values[half];
+  if (values.length % 2)
+    return values[half];
   else
-      return (values[half-1] + values[half]) / 2.0;
+    return (values[half - 1] + values[half]) / 2.0;
 }
