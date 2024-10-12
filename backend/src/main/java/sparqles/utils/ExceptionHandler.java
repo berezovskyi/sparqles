32807e29c778a5a8f0e9eaff769e76bc233788fe
Andrew Berezovskyi <andrew@berezovskyi.me>
2024-10-12 23:02:50 +0200

    Backend logging improvements (#59)

--- backend/src/main/java/sparqles/utils/ExceptionHandler.java
+++ backend/src/main/java/sparqles/utils/ExceptionHandler.java
@@ -1,5 +1,6 @@
 package sparqles.utils;
 
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -66,6 +67,9 @@ public class ExceptionHandler {
     }
     
     public static String getExceptionSummary(String message) {
+        if (StringUtils.isBlank(message)) {
+            return "N/A";
+        }
         int cutoff = message.indexOf('\n');
         if (cutoff == -1 ) {
             cutoff = message.length();
